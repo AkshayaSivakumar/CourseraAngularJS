e8a9d20ba5b237cd8b3940c286cfbdf78a02d9d2
Akshaya Sivakumar <buvanaksh@gmail.com>
2021-04-05 21:07:06 +0530

    Protecting DI from Minification Example

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 (function () {
 'use strict';
 
+//Protecting Dependency Injection from Minification
+
+//Explicitly mentioning function arguments
+// angular.module('DIApp', []).controller('DIController',['$scope', '$filter', DIController]);
+//
+// function DIController($scope, $filter){
+//   $scope.name = "AngularJS";
+//
+//   $scope.upper = function(){
+//     var upCase = $filter('uppercase');
+//     $scope.name = upCase($scope.name);
+//   };
+// }
+
+//Using Inline function
+// angular.module('DIApp', []).controller('DIController', ['$scope', '$filter', function($scope, $filter){
+//   $scope.name = "AngularJS";
+//
+//   $scope.upper = function(){
+//     var upCase = $filter('uppercase');
+//     $scope.name = upCase($scope.name);
+//   };
+// }]);
+
+//Adding an extra property to controller function value
 angular.module('DIApp', []).controller('DIController', DIController);
 
-function DIController($scope, $filter, $injector){
+DIController.$inject = ['$scope', '$filter'];
+
+function DIController($scope, $filter){
   $scope.name = "AngularJS";
 
   $scope.upper = function(){
     var upCase = $filter('uppercase');
     $scope.name = upCase($scope.name);
   };
-
-  console.log($injector.annotate(DIController));
 }
-
-console.log(DIController.toString());
 
 })();
